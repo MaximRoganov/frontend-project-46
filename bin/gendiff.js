bf08820c3fd0c5466ce662063980f77559961c24
Maksim Roganov <total.on.off@gmail.com>
2022-11-24 10:18:45 +0300

    option and arguments were added to gendiff

--- bin/gendiff.js
+++ bin/gendiff.js
@@ -8,5 +8,21 @@ program
   .description('Compares two configuration files and shows a difference.')
   .version('0.0.0.1');
 
+program
+  .option('-f, --format <type>','output format');
+
+program
+  .argument('<filepath1>')
+  .argument('<filepath2>')
+  .action((filepath1, filepath2) => {
+    console.log('filepath1:', filepath1);
+    console.log('filepath2:', filepath2);
+  });
 
   program.parse();
+
+  const options = program.opts();
+
+  if (options.format){
+    console.log(options)
+  };
